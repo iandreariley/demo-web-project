d3e92cce73bdbcc5e46ebb494d96243d02b76f46
iandreariley <iandreariley@gmail.com>
2016-01-18 16:04:14 -0800

    Add a form and API for uploading a file.

--- src/main/java/edu/cpp/cs580/controller/WebController.java
+++ src/main/java/edu/cpp/cs580/controller/WebController.java
@@ -1,5 +1,8 @@
 package edu.cpp.cs580.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.cpp.cs580.App;
@@ -25,6 +30,36 @@ import edu.cpp.cs580.data.provider.UserManager;
 
 @RestController
 public class WebController {
+	
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + "!";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+
+	/**
+	 * When the class instance is annotated with
+	 * {@link Autowired}, it will be looking for the actual
+	 * instance from the defined beans.
+	 * <p>
+	 * In our project, all the beans are defined in
+	 * the {@link App} class.
+	 */
+	@Autowired
+	private UserManager userManager;
 
 	/**
 	 * This is a simple example of how the HTTP API works.
